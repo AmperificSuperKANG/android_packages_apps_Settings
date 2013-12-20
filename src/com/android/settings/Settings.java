48b60f0f5b70c156da8af4a6e0cfa70d7e49f51e
Grarak <williye97@gmail.com>
2013-12-20 14:40:31 +0100

    Revert "Settings: Add bridge to PerformanceControl"
    This reverts commit c859267abffbea09b61f71f5be7d81ac420a82e2.
    
    Signed-off-by: Grarak <williye97@gmail.com>
    
    Conflicts:
    	res/drawable-hdpi/ic_settings_performance.png
    	res/drawable-mdpi/ic_settings_performance.png
    	res/drawable-xhdpi/ic_settings_performance.png
    	res/values/custom_strings.xml

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -601,10 +601,6 @@ public class Settings extends PreferenceActivity
                 if (!showDev) {
                     target.remove(i);
                 }
-            } else if (id == R.id.performance_controls) {
-                if (!showDev) {
-                    target.remove(i);
-                }
             } else if (id == R.id.account_add) {
                 if (um.hasUserRestriction(UserManager.DISALLOW_MODIFY_ACCOUNTS)) {
                     target.remove(i);
@@ -1134,5 +1130,4 @@ public class Settings extends PreferenceActivity
     public static class PaymentSettingsActivity extends Settings { /* empty */ }
     public static class PrintSettingsActivity extends Settings { /* empty */ }
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
-    public static class PerformanceSettingsActivity extends Settings { /* empty */ }
 }
